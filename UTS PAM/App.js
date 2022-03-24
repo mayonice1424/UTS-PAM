9d58263a1bb41acc7f6044d630ece081a8347bb7
mayonice1424 <friyandrailham14@gmail.com>
2022-03-25 03:02:06 +0700

    create a file

---
+++ UTS PAM/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const App = () => {
+  return (
+    <View>
+      <FontAwesome5 name="home" />
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
